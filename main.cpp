5dfd7b2b625951cdba8059e07d73afb018adff3c
root <root@DESKTOP-I0F9LL8>
2025-04-19 14:15:49 +0300

    some addition to constructors and to others

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 int main(){
-    const int soa = 5;
-    double* teto =  new double[soa];
-    TetoPolynomial poly(teto,soa);
+    // const int soa = 5;
+    // double* teto =  new double[soa];
+    TetoPolynomial poly({1.2});
     cout << poly.getLength() << endl;
     return 0;
 }
